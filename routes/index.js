7b228548e70350624422fbbe31b1f9623ab74b55
Atoti2 <bogyikormi3@gmail.com>
2024-03-14 16:11:15 +0100

    completed

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard',  messages: messages});
+});
+
+router.post('/new', function(req, res, next){
+  const name = req.body.name
+  const message = req.body.message
+  messages.push({text: message, user: name, added: new Date()})
+  res.redirect('/')
+})
+
+
+module.exports = router;
